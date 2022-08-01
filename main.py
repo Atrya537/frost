115fc4c101bab13f376de77311b84eae1a8ba277
Atrya537 <101434393+Atrya537@users.noreply.github.com>
2022-08-01 17:44:10 -0400

    Got notifications to work

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 import psutil
 import os
+from plyer import notification
+import math
+import time
 
-print('Cpu Usage: ', psutil.cpu_percent(4))
+while True:
+    cpu_usage = math.floor(psutil.cpu_percent(3))
+
+    if cpu_usage > 25:
+        notification.notify(
+            title = 'CPU Usage Alert',
+            message = str(cpu_usage),
+            app_icon = None,
+            timeout = 2
+        )
+        time.sleep(5)
